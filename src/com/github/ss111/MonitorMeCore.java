ccbb6777dbfa5d514cde2340c2650f60bd779ee0
Sam Mauck <smmauck@gmail.com>
2013-02-02 14:24:34 -0700

    Add version TEST_2 to master branch

--- src/com/github/ss111/MonitorMeCore.java
+++ src/com/github/ss111/MonitorMeCore.java
@@ -1,4 +1,7 @@
 package com.github.ss111;
+import java.net.*;
+import java.io.*;
+
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,12 +12,22 @@ public final class MonitorMeCore extends JavaPlugin
 	{
 		final FileConfiguration config = this.getConfig();
 		config.addDefault("Password", "daef4953b9783365cad6615223720506cc46c5167cd16ab500fa597aa08ff964eb24fb19687f34d7665f778fcb6c5358fc0a5b81e1662cf90f73a2671c53f991");
-		config.addDefault("Port", "3002");
+		config.addDefault("Port", 3002);
 		config.options().header("NOTE: To make your password, go to http://www.fileformat.info/tool/hash.htm and get the SHA-512 hash! The default password is \"test123\"");
 		config.options().copyDefaults(true);
 		saveConfig();
 		
 		getLogger().info("MonitorMe loaded! Starting mini-server...");
+		
+		try
+		{
+			ServerSocket ss = new ServerSocket(config.getInt("Port"));
+		} 
+		catch (IOException e)
+		{
+			getLogger().severe("MonitorMe cannot listen on port " + config.getInt("Port") + "! Is the port correctly forwarded?");
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
